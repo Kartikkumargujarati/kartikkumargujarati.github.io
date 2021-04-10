50522f1fe6fc6a769dbff0d7242959b745c9e5da
kartikkumargujarati <kgujarati210@gmail.com>
2021-04-10 17:52:26 -0500

    Updated sidebar items alignment to center

--- src/@wkocjan/gatsby-theme-intro/components/sidebar/sidebar.js
+++ src/@wkocjan/gatsby-theme-intro/components/sidebar/sidebar.js
@@ -8,26 +8,29 @@ const Sidebar = ({ profile, social }) => (
   <aside className="w-full lg:w-1/3 lg:border-r border-line lg:px-6 xl:px-12">
     <div className="flex flex-col h-full justify-between">
       <div>
-        <h1 className="font-header font-black text-front text-5xl leading-none break-words mb-6">
+        <h1 className="font-header font-black text-front text-5xl leading-none break-words mb-6 text-center">
           {profile.name}
         </h1>
         {profile.image && (
           <ProfileImage image={profile.image} name={profile.name} />
         )}
         <br />
+        <h4 className="text-medium text-center">
+          Tech Enthusiast | Achiever | Restorative | Learner | Analytical
+        </h4>
+        <div className="flex justify-center inline-block">
         {profile.location && (
           <Location
             location={profile.location}
             relocation={profile.relocation}
           />
         )}
+        </div>
+        <br />
+        <div className="flex justify-center inline-block ">
+          <br />
+          <SocialLinks social={social} />
       </div>
-
-      <div className="pt-8 pb-12 lg:py-0">
-        <h5 className="font-header font-semibold text-front text-sm uppercase mb-3">
-          Connect
-        </h5>
-        <SocialLinks social={social} />
       </div>
     </div>
   </aside>
