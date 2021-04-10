ff6eb3fb3c9e27ac8b3598161184ba1906d68178
kartikkumargujarati <kgujarati210@gmail.com>
2021-04-10 16:17:05 -0500

    Shaowing the sidebar profile section

---
+++ src/@wkocjan/gatsby-theme-intro/types/social.js
@@ -0,0 +1,21 @@
+import { graphql } from "gatsby"
+import { string, oneOf } from "prop-types"
+
+export const SocialType = {
+  name: oneOf([
+    "behance",
+    "dribbble",
+    "facebook",
+    "github",
+    "goodreads",
+    "instagram",
+    "linkedin",
+    "medium",
+    "producthunt",
+    "twitter",
+    "youtube",
+    "xing",
+  ]),
+  url: string,
+}
+
