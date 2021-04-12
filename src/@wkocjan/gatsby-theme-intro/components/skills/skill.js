95193e07433ff276cbcba2377b7bab0418023b1c
kartikkumargujarati <kgujarati210@gmail.com>
2021-04-11 19:05:52 -0500

    adding border solid prop

--- src/@wkocjan/gatsby-theme-intro/components/skills/skill.js
+++ src/@wkocjan/gatsby-theme-intro/components/skills/skill.js
@@ -4,7 +4,7 @@ import SkillSvg from "./skill.svg"
 
 const Skill = ({ skill, i }) => (
   <li
-    className={`relative flex justify-center items-center rounded-full border-2 border-skill-${i}`}
+    className={`relative flex justify-center items-center rounded-full border-solid border-2 border-skill-${i}`}
   >
     <span className="absolute font-header font-semibold text-front text-sm md:text-2xl px-2 text-center">
       {skill}
