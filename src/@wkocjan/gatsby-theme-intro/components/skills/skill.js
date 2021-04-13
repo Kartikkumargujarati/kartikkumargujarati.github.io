b8a62f9b2ccfc634c79b175ce9aadfce1ec4f342
kartikkumargujarati <kgujarati210@gmail.com>
2021-04-12 22:24:59 -0500

    using the skill css for colors

--- src/@wkocjan/gatsby-theme-intro/components/skills/skill.js
+++ src/@wkocjan/gatsby-theme-intro/components/skills/skill.js
@@ -1,15 +1,16 @@
 import React from "react"
 import { number, string } from "prop-types"
 import SkillSvg from "./skill.svg"
+import "./skill.css"
 
 const Skill = ({ skill, i }) => (
   <li
-    className={`relative flex justify-center items-center rounded-full border-solid border-2 border-skill-${i}`}
+    className={`relative flex justify-center items-center rounded-full border-solid border-2 skill-entry-${i}`}
   >
     <span className="absolute font-header font-semibold text-front text-sm md:text-2xl px-2 text-center">
       {skill}
     </span>
-    <SkillSvg className={`w-full text-skill-${i} opacity-75 m-2`} />
+    <SkillSvg className={`w-full opacity-75 m-2 skill-entry-${i}`} />
   </li>
 )
 
